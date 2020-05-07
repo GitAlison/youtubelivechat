ae01936f871e86ab9c99e992ff09646bf53bbea5
Alison Aguiar <alysson.k888@gmail.com>
2020-05-07 19:22:17 -0300

    add user token on request

--- frontend/src/app/services/room.service.ts
+++ frontend/src/app/services/room.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
 import { HttpClient } from '@angular/common/http';
 import { Message } from '../store/models/message.model';
+import { Observable } from 'rxjs';
+import { AuthState, getAuthState } from '../auth/store/auth/auth.reducer';
+import { Store } from '@ngrx/store';
+import { Token } from '../auth/store/auth/user.model';
 
 @Injectable({
   providedIn: 'root',
@@ -9,8 +13,17 @@ import { Message } from '../store/models/message.model';
 export class RoomService {
   socket;
   api = 'http://127.0.0.1:3000/room';
-
-  constructor(private http: HttpClient, private socketClient: Socket) {}
+  // authState: Observable<AuthState>;
+  token: Token;
+  constructor(
+    private store: Store<AuthState>,
+    private http: HttpClient,
+    private socketClient: Socket
+  ) {
+    // this.store.select(getAuthState).subscribe((data) => {
+    //   this.token = data.token;
+    // });
+  }
 
   connnect() {
     this.socketClient.ioSocket.io.uri = 'http://localhost:3000'; //new uri
@@ -27,7 +40,7 @@ export class RoomService {
     this.socketClient.emit('room', {
       type: 'send_message',
       headers: {
-        jwt: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFsaXNvbiIsInN1YiI6MSwiaWF0IjoxNTg4Nzk2MDA0LCJleHAiOjE1ODg4OTYwMDN9._TtaItKK_V-kiYRduGKd2jwMaqbO-RBEt8AXxtiTXvk`,
+        jwt: `${localStorage.getItem('tokenAccess')}`,
       },
       content: {
         text: text,
