4ae7e4c276baf5c3b6eda61704381c6d78d72464
Alison Aguiar <alysson.k888@gmail.com>
2020-05-10 15:54:05 -0300

    change route prod

--- frontend/src/app/auth/services/auth.service.ts
+++ frontend/src/app/auth/services/auth.service.ts
@@ -2,23 +2,25 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 import { Token } from '../store/auth/user.model';
+import { environment } from 'src/environments/environment';
 
-enum AuthRoutes {
-  LOGIN = 'http://localhost:3000/auth/login',
-  REGISTER = 'http://localhost:3000/auth/register',
-}
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+
+  LOGIN = environment.base_api + 'auth/login';
+  REGISTER = environment.base_api +'auth/register';
+
   constructor(private http: HttpClient) {}
 
   login(form: any) {
-    return this.http.post<Token>(AuthRoutes.LOGIN, form);
+    return this.http.post<Token>(this.LOGIN, form);
   }
 
   register(form: any) {
-    return this.http.post<Token>(AuthRoutes.REGISTER, form);
+    return this.http.post<Token>(this.REGISTER, form);
   }
 }
