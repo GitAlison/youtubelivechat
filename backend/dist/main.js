f86f5ea8636c6dbbb22ee075678a06440b410a4a
Alison Aguiar <alysson.k888@gmail.com>
2020-05-10 14:03:22 -0300

    deploy app

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -10,7 +10,7 @@ async function bootstrap() {
     };
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: false });
     app.use(cors(corsOptions));
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
